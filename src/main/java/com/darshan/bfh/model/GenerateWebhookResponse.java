02d5cee66d024427b23daee850a88e4aca98f95d
darshangowdagl <darshandarshugl101218@gmail.com>
2025-11-11 16:23:03 +0530

    Add GenerateWebhookResponse model to capture webhook URL and JWT token

---
+++ src/main/java/com/darshan/bfh/model/GenerateWebhookResponse.java
@@ -0,0 +1,27 @@
+package com.darshan.bfh.model;
+
+public class GenerateWebhookResponse {
+	private String webhook;
+	private String accessToken;
+
+	public GenerateWebhookResponse() {
+	}
+
+	public String getWebhook() {
+		return webhook;
+	}
+
+	public void setWebhook(String webhook) {
+		this.webhook = webhook;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+}
+
+
