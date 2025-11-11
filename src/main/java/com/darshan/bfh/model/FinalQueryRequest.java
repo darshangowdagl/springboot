90a8c9b5792f520b6db174ee0c387fcd296b20ea
darshangowdagl <darshandarshugl101218@gmail.com>
2025-11-11 16:23:08 +0530

    Add FinalQueryRequest model for submitting final SQL query payload

---
+++ src/main/java/com/darshan/bfh/model/FinalQueryRequest.java
@@ -0,0 +1,22 @@
+package com.darshan.bfh.model;
+
+public class FinalQueryRequest {
+	private String finalQuery;
+
+	public FinalQueryRequest() {
+	}
+
+	public FinalQueryRequest(String finalQuery) {
+		this.finalQuery = finalQuery;
+	}
+
+	public String getFinalQuery() {
+		return finalQuery;
+	}
+
+	public void setFinalQuery(String finalQuery) {
+		this.finalQuery = finalQuery;
+	}
+}
+
+
